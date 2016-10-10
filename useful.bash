78ebb8384976f369f7bbb44532a3e924d4a6a3a9
juno <juno@krasava.fi>
2016-10-10 07:24:34 +0300

    dev

--- useful.bash
+++ useful.bash
@@ -6,4 +6,4 @@ kubectl run huoneisto-utils --image=gcr.io/jntlserv0/huoneisto_utils:0.2 --env="
 
 kubectl delete pods $( kubectl get pods --show-all |grep 'Completed' |awk '{print $1}')
 
-kubectl delete pods $( kubectl get pods --show-all |grep 'Completed' |awk '{print $1}') && kubectl delete jobs pi && kubectl create -f job_espoo.huoneisto.mobi.yaml
+kubectl delete pods $( kubectl get pods --show-all |grep 'Completed' |awk '{print $1}') && kubectl delete jobs pi && kubectl create -f job_espoo.huoneisto.mobi.yaml && kubectl create -f job_www.huoneisto.mobi.yaml && kubectl create -f job_huoneisto.mobi.yaml
