41293e8ce0fe8f2ff255722a0bebc97471ceb2e2
juno <juno@krasava.fi>
2016-10-12 06:01:14 +0300

    dev

--- useful.bash
+++ useful.bash
@@ -2,7 +2,7 @@
 
 kubectl run huoneisto-utils --image=gcr.io/jntlserv0/huoneisto_utils:0.2 --env="THEMES=realestate" --env="LOCALE=fi_FI" --env=""  --env=""  --env=""  --env=""  --env=""
 
-
+kubectl get pods -o json | jq  '.items[] | select( .status.phase == "Succeeded")|.metadata.name'
 
 kubectl delete pods $( kubectl get pods --show-all |grep 'Completed' |awk '{print $1}')
 
